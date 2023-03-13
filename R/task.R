f5d1f686149fef6ee1fa8628bd984d1964da503a
matt.ogden <mogd001@gmail.com>
2023-03-14 08:30:19 +1300

    Replacing imports, trying to get basic workflow working

--- R/task.R
+++ R/task.R
@@ -1,4 +1,5 @@
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
 library(lubridate)
 
 x <- rnorm(1:10)
@@ -12,3 +13,20 @@ p <- ggplot(df, aes(x,y)) +
 fname <- paste0("outputs/data_", make.names(Sys.time()), ".png")
 
 ggsave(fname, p, dpi = 300, width = 5, height = 5)
+
+
+# # Exploring upload to sharepoint
+# library(glue)
+# library(Microsoft365R)
+#
+# list_sharepoint_sites()
+#
+# site <- get_sharepoint_site(site_name = "Environmental Monitoring")
+#
+#
+#
+# site$list_drives()
+#
+# site$get_drive()$list_items("Reports and Analyses")
+#
+# site$get_drive()$upload_file(glue("{fname}"), glue("Reports and Analyses/R Outputs/{fname}"))
